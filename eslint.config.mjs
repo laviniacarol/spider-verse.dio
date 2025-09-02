30c01d12025265fe7341ba8eb1375ac4d7f4320a
carol <carolxlavinia@gmail.com>
2025-09-02 17:18:59 -0300

    adding files structure, images, icons, sounds and json

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier"
+  ),
+
+  {
+    plugins: ["unused-imports", "import"], 
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "import/order": [
+        "error",
+        { "newlines-between": "always"}
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
