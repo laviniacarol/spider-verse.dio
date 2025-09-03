db370629a2202452c8c05ecb662faa5fedbad7f5
carol <carolxlavinia@gmail.com>
2025-09-02 22:55:40 -0300

    adding first file scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,41 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import Image from 'next/image';
+import "./globals.scss";
 
-const inter = Inter({
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Spiderverse",
-  description: "Carrosel parallax Aranhaverso com React, Next.js e Framer Motion",
+  description:
+    "Carrosel parallax Aranhaverso com React, Next.js e Framer Motion",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="pt-BR">
+      <body>
+        <header>
+          <Image
+            src="/icons/menu.svg"
+            alt="Opções de menu"
+            width={36}
+            height={25}
+          />
+          <Image
+          src="/spider-logo.svg" 
+          alt="spider-man" 
+          width={260}
+          height={70}
+          />
+              <Image
+            src="/icons/user.svg"
+            alt="Login"
+            width={36}
+            height={36}
+          />
+        </header>
+
         {children}
       </body>
     </html>
