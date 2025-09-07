99b741997b13df36ccb7da9c36edb0de90a46072
carol <carolxlavinia@gmail.com>
2025-09-07 19:56:25 -0300

    adding images and grabbing effect to carrousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,5 +1,11 @@
+"use client"
+
 import { IHeroData } from "@/interfaces/heroes"
 import HeroDetails from "../HeroDetails";
+import styles from "./carousel.module.scss";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import HeroPicture from "../HeroPicture";
 
 interface IProps {
     heroes: IHeroData[];
@@ -7,8 +13,47 @@ interface IProps {
 }
 
 export default function Carousel({heroes, activeId}: IProps) {
-  return <>
-  <h1>Componente Carousel: {activeId}</h1>
-  <HeroDetails data={heroes[0]} />
-  </>
+   const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null);
+   const [activeIndex, setActiveIndex] = useState<number>(
+    heroes.findIndex((hero) => hero.id === activeId)
+   );
+
+   useEffect(() => {
+    const indexInArrayScope = ((activeIndex % heroes.length) + heroes.length) % heroes.length;
+
+    const visibleItems = [...heroes, ...heroes].slice(indexInArrayScope, indexInArrayScope + 3);
+   setVisibleItems(visibleItems)
+  }, [heroes, activeIndex]);
+   
+
+  //Altera entre os heróis do carrosel
+  // +1 muda no sentido horário
+  // -1 muda no sentido anti-horário
+   const handleChangeActiveIndex = (newDirection: number) => {
+    setActiveIndex((prevActiveIndex) => prevActiveIndex + newDirection);
+   }
+   
+
+   if (!visibleItems) {
+    return null;
+   }
+
+  return (
+  <div className={styles.container}>
+    <div className={styles.carousel}>
+       <div className={styles.wrapper} onClick={() => handleChangeActiveIndex(1)}>
+        {visibleItems.map((item) => (
+            <div key={item.id} className={styles.hero}>
+               <HeroPicture hero={item} />
+            </div>
+        ))} 
+       </div>
+      </div>
+
+    <div className={styles.details}>
+    <HeroDetails data={heroes[0]} />
+    </div>
+  </div>
+  );
 }
+
