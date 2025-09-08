78cddaa02e3c582b4e1b74b3095f714d38d2fefb
carol <carolxlavinia@gmail.com>
2025-09-07 22:10:49 -0300

    carousel final result

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,59 +1,108 @@
-"use client"
+"use client";
 
-import { IHeroData } from "@/interfaces/heroes"
+import { IHeroData } from "@/interfaces/heroes";
 import HeroDetails from "../HeroDetails";
 import styles from "./carousel.module.scss";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import HeroPicture from "../HeroPicture";
+import { AnimatePresence, motion, scale } from "framer-motion";
+import { filter } from "framer-motion/client";
 
-interface IProps {
-    heroes: IHeroData[];
-    activeId: string;
+
+enum enPosition {
+  FRONT = 0,
+  MIDDLE = 1,
+  BACK = 2,
 }
 
-export default function Carousel({heroes, activeId}: IProps) {
-   const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null);
-   const [activeIndex, setActiveIndex] = useState<number>(
+interface IProps {
+  heroes: IHeroData[];
+  activeId: string;
+}
+
+export default function Carousel({ heroes, activeId }: IProps) {
+  const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(
     heroes.findIndex((hero) => hero.id === activeId)
-   );
+  );
 
-   useEffect(() => {
-    const indexInArrayScope = ((activeIndex % heroes.length) + heroes.length) % heroes.length;
+  useEffect(() => {
+    const indexInArrayScope =
+      ((activeIndex % heroes.length) + heroes.length) % heroes.length;
 
-    const visibleItems = [...heroes, ...heroes].slice(indexInArrayScope, indexInArrayScope + 3);
-   setVisibleItems(visibleItems)
+    const visibleItems = [...heroes, ...heroes].slice(
+      indexInArrayScope,
+      indexInArrayScope + 3
+    );
+    setVisibleItems(visibleItems);
   }, [heroes, activeIndex]);
-   
 
   //Altera entre os heróis do carrosel
   // +1 muda no sentido horário
   // -1 muda no sentido anti-horário
-   const handleChangeActiveIndex = (newDirection: number) => {
+  const handleChangeActiveIndex = (newDirection: number) => {
     setActiveIndex((prevActiveIndex) => prevActiveIndex + newDirection);
-   }
-   
+  };
 
-   if (!visibleItems) {
+  if (!visibleItems) {
     return null;
-   }
+  }
 
   return (
-  <div className={styles.container}>
-    <div className={styles.carousel}>
-       <div className={styles.wrapper} onClick={() => handleChangeActiveIndex(1)}>
-        {visibleItems.map((item) => (
-            <div key={item.id} className={styles.hero}>
-               <HeroPicture hero={item} />
-            </div>
-        ))} 
-       </div>
+    <div className={styles.container}>
+      <div className={styles.carousel}>
+        <div
+          className={styles.wrapper}
+          onClick={() => handleChangeActiveIndex(1)}
+        >
+          <AnimatePresence mode="popLayout">
+            {visibleItems.map((item, position) => (
+              <motion.div key={item.id} 
+              className={styles.hero} 
+              initial={{ x: -1500, scale: 0.75}}
+              animate={{x: 0, ...getItemStyles(position)}}
+              exit={{x:0, opacity: 0, scale: 1, left: "-20%"}}
+              transition={{ duration: 0.8}}>
+                <HeroPicture hero={item} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
       </div>
 
-    <div className={styles.details}>
-    <HeroDetails data={heroes[0]} />
+      <div className={styles.details}>
+        <HeroDetails data={heroes[0]} />
+      </div>
     </div>
-  </div>
   );
 }
 
+const getItemStyles = (position: enPosition) => {
+     if (position === enPosition.FRONT) {
+      return {
+        zIndex: 3,
+        filter: "blur(10px)",
+        scale: 1.2,
+      }
+     }
+
+     if (position === enPosition.MIDDLE) {
+      return {
+        zIndex: 2,
+        left: 300,
+        scale: 0.8,
+        top: "-10%",
+      }
+     }
+
+     return {
+      zIndex: 1,
+      filter: "blur(10px)",
+      left: 160,
+      top: "-20%",
+      scale: 0.6,
+      opacity: 0.8,
+     }
+
+}
