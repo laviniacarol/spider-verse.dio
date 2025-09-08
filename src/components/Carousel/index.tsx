702cb2771213b49bd73290742b1ff806a3d6e7f4
carol <carolxlavinia@gmail.com>
2025-09-07 23:37:01 -0300

    final commit

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -95,8 +95,24 @@ export default function Carousel({ heroes, activeId }: IProps) {
     if (!startInteractionPosition) {
       return null;
     }
+    handleChangeDragTouch(e.clientX);
+  };
 
-    const endInteractionPosition = e.clientX;
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+     setStartInteractionPosition(e.touches[0].clientX);
+  }
+
+  const handleTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+       if (!startInteractionPosition) {
+      return null;
+    }
+
+    handleChangeDragTouch(e.changedTouches[0].clientX)
+  }
+
+
+  const handleChangeDragTouch = (clientX: number) => {
+    const endInteractionPosition = clientX;
     const diffPosition = endInteractionPosition - startInteractionPosition
     const newPosition = diffPosition > 0 ? -1 : 1;
 
@@ -121,6 +137,8 @@ export default function Carousel({ heroes, activeId }: IProps) {
           className={styles.wrapper}
           onDragStart={handleDragStart}
           onDragEnd={handleDragEnd}
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
         >
           <AnimatePresence mode="popLayout">
             {visibleItems.map((item, position) => (
